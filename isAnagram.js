3395f398e2cc7d368e6d688963e62dff265a4f77
praveen <praveen3791@gmail.com>
2021-12-29 12:38:07 +0530

    isAnangram

--- isAnagram.js
+++ isAnagram.js
@@ -1,6 +1,33 @@
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
+    // const sArray = [...s];
+    // const tArray = [...t];
+    // if (sArray.length != tArray.length) {
+    //     return false;
+    // }
+    // if(JSON.stringify(sArray.sort()) === JSON.stringify(tArray.sort())){
+    //     return true
+    // }
+    // else return false;
+ if(s.length != t.length) return false;
     
+    var arr1 = s.split('');
+    var arr2 = t.split('');
+    var f1 = {};
+    
+    arr1.forEach(a => {
+        if(!f1[a]) f1[a] = 0;
+        f1[a]++;
+    });
+    
+    arr2.forEach(a => {
+        if(!f1[a]) return false;
+        f1[a] -= 1;
+        if(!f1[a]) delete f1[a];
+    });
+        
+    return !Object.keys(f1).length;
+
 };
 const s = "anagram";
 const t = "nagaram";
-console.log(isAnagram(s,t))
+console.log(isAnagram(s, t))
