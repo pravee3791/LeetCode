341764edf1a99e0d4a1087c34df71f132089ef0f
pravee3791 <praveen3791@gmail.com>
2022-01-07 21:37:01 +0530

    solved some more exercise

--- moveZeroes.js
+++ moveZeroes.js
@@ -3,7 +3,16 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] !=0){
+            const temp = nums[j];
+            nums[j] = nums[i]
+            nums[i] = temp;
+            j++;
+        }
+    }
+    return nums;
 };
 const nums = [0,1,0,3,12];
 console.log(moveZeroes(nums));
