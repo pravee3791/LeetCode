a5507a54b552757386e7bbeb7869afe4293dfd13
praveen <praveen3791@gmail.com>
2022-01-08 19:20:58 +0530

    solving more problems

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -2,29 +2,59 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var removeDuplicates = function(nums) {
+//     let currentNumber = undefined;
+//     let count = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if(currentNumber != undefined && nums[i]==currentNumber){
+//             nums[i] ='_';
+//         }
+//         else{
+//             currentNumber = nums[i];
+//         }
+//     }
+//     let j = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if(nums[i] != '_'){
+//             const temp = nums[j]
+//             nums[j] = nums[i];
+//             nums[i] = temp;
+//             j++;
+//         }
+//     }
+//     for (let i = 0; i < nums.length; i++) {
+//        if (nums[i] == '_') {
+//            count = i;
+//            return count;
+//        } 
+//     }
+//     console.log(nums)
+//    return count;
+// };
+
 var removeDuplicates = function(nums) {
-    let currentNumber = undefined;
-    for (let i = 0; i < nums.length; i++) {
-        if(currentNumber != undefined && nums[i]==currentNumber){
-            nums[i] ='';
+    let count = 1;
+    let currentNumber = nums[0]
+    let j = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if(nums[i] != currentNumber ) {
+            currentNumber = nums[i];
+            count++;
         }
         else{
-            currentNumber = nums[i];
+            nums[i] ="_"
         }
     }
-    let currentEmptyIndex = undefined;
-    let isValueSwapped = false; 
     for (let i = 0; i < nums.length; i++) {
-        if(nums[i] === '' && isValueSwapped== false){
-            currentEmptyIndex = i;
+        if(nums[i] != '_'){
+            const temp = nums[j]
+            nums[j] = nums[i];
+            nums[i] = temp;
+            j++;
         }
-        else if(nums[i] != ''){
-            nums[currentEmptyIndex] = nums[i];
-            nums[i] ="";
-        }
-
     }
-    // return nums;
-};
+    return count;
+}
 const nums = [0,0,1,1,1,2,2,3,3,4]
+// const nums = [1]
 console.log(removeDuplicates(nums))
--- reverseInteger.js
+++ reverseInteger.js
@@ -1,8 +1,32 @@
 var reverse = function(x) {
-    console.log(x);
-    let y = String(x);
-    let j = y.split('').reverse();
-    console.log(j.join(''));
+    let n;
+    let isNegative = false ;
+    let reverseInterger;
+    if (x < 0) {
+       n = Math.abs(x) 
+       isNegative = true;
+    } else {
+        n = x;
+    }
+    n = n.toString().split('');
+    let i = 0;
+    let j = n.length -1;
+    while (i<j) {
+       let temp = n[i];
+        n[i] = n[j]
+        n[j] = temp;
+        i++;
+        j--; 
+    }
+    if(isNegative){
+       reverseInterger =  -Math.abs(n.join(''))
+    }
+    else{
+        reverseInterger = n.join('');
+    }
+    if (reverseInterger < (Math.pow(2, 31) * -1) || reverseInterger > Math.pow(2, 31) - 1) return 0;
+    return reverseInterger;
 };
 let x = 123;
-reverse(x)
+// const x = -123;
+console.log(reverse(x));
